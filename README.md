39b8be4c65cc832a5a69590678da0e44c931abe0
heshansq <108335900+heshansq@users.noreply.github.com>
2023-06-08 21:22:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-assignment-new
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-gor9sp)
---
+++ index.ts
@@ -0,0 +1,80 @@
+// Import stylesheets
+// Extend this type to make it support the usage below.
+// path should be recieved in dot notation and should give suggestions on keys.
+// correct return type should be calculated depending on the given path.
+
+/**
+ * ValueTypeInPath: type which will use to calculate the return type of get method
+ */
+type ValueTypeInPath<T, P extends string> = P extends keyof T
+  ? T[P]
+  : P extends `${infer Key}.${infer Rest}`
+  ? Key extends keyof T
+    ? ValueTypeInPath<T[Key], Rest>
+    : never
+  : never;
+
+/**
+ * NestedKeys: type which use to get nested keys and provide suggetion on path
+ * TODO: Getting an error "NestedKeys<T[Extract<keyof T, string>]" recursive implementation
+ */
+type NestedKeys<T> = T extends object
+  ?
+      | keyof T
+      | `${Extract<keyof T, string>}.${NestedKeys<T[Extract<keyof T, string>]>}`
+  : never;
+
+/**
+ * getKeyValue: a recursive function outside of main get function to get value of the given path
+ */
+function getKeyValue<T, P>(currentObject: T, currentKey: P): T {
+  return (
+    currentObject !== undefined && currentObject?.[currentKey as keyof T]
+      ? currentObject?.[currentKey as keyof T]
+      : undefined
+  ) as T;
+}
+
+//declare function get<T, P extends NestedKeys<T> & string>(obj: {}, path: string): ValueTypeInPath<T, P>;
+
+/**
+ * get: get method with generic types
+ */
+function get<T, P extends NestedKeys<T> & string>(
+  obj: T,
+  path: P
+): ValueTypeInPath<T, P> {
+  return path.split('.')?.reduce(getKeyValue, obj) as ValueTypeInPath<T, P>;
+}
+
+const object = {
+  user: {
+    fullName: 'Janis Pagac',
+    age: 30,
+    address: {
+      street: '83727 Beatty Garden',
+      city: 'Hamilton',
+    },
+    friends: {
+      '0': {
+        fullName: 'Franklin Kuhn',
+        age: 20,
+      },
+      '1': {
+        fullName: 'Hubert Sawayn',
+      },
+    },
+  },
+};
+
+const fullName = get(object, 'user.fullName');
+const age = get(object, 'user.age');
+const street = get(object, 'user.address.street');
+const fullNameOfFriend = get(object, 'user.friends.0.fullName');
+const ageOfFriend = get(object, 'user.friends.0.age');
+
+console.log({ fullName });
+console.log({ age });
+console.log({ street });
+console.log({ fullNameOfFriend });
+console.log({ ageOfFriend });
